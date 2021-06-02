32c2b9dc24caf7048a983dc6179d0216643c95c1
Sofia Leon <sofialeon@google.com>
2021-06-02 12:22:23 -0500

    add to gitignore

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -27,16 +27,14 @@ async function main() {
 
   // Creates a client
   // eslint-disable-next-line no-unused-vars
-  const client = new {CloudShellServiceClient}();
+  const client = new CloudShellServiceClient();
 
   //TODO(library generator): write the actual function you will be testing
-  async function doSomething() {
-   console.log('DPE! Change this code so that it shows how to use the library! See comments below on structure.')
-   // const [thing] = await client.methodName({
-   // });
-   // console.info(thing);
+  async function initializeClient() {
+    const operation = await client.initialize();
+    console.info(operation);
   }
-  doSomething();
+  initializeClient();
   // [END nodejs_cloud_shell_quickstart]
 }
 
